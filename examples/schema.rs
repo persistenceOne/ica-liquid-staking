c513d5bca8893093ecdb964a05f279bab5d52441
Nabarun Sarkar <nabsarkar@gmail.com>
2023-12-11 14:58:27 +0400

    Initial Commit

---
+++ examples/schema.rs
@@ -0,0 +1,11 @@
+use cosmwasm_schema::write_api;
+
+use ica_liquid_staking::msg::{ExecuteMsg, InstantiateMsg, QueryMsg};
+
+fn main() {
+    write_api! {
+        instantiate: InstantiateMsg,
+        execute: ExecuteMsg,
+        query: QueryMsg,
+    }
+}
