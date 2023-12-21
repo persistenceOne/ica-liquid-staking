835d6d3358c37c1f21a14d89f62568f87eec8efd
Nabarun Sarkar <nabsarkar@gmail.com>
2023-12-21 17:13:29 +0400

    Add interchaintest

---
+++ interchaintest/ibc_transfer_test.go
@@ -0,0 +1,164 @@
+package interchaintest
+
+import (
+	"context"
+	"testing"
+
+	"cosmossdk.io/math"
+	transfertypes "github.com/cosmos/ibc-go/v7/modules/apps/transfer/types"
+	"github.com/strangelove-ventures/interchaintest/v7"
+	"github.com/strangelove-ventures/interchaintest/v7/chain/cosmos"
+	"github.com/strangelove-ventures/interchaintest/v7/ibc"
+	interchaintestrelayer "github.com/strangelove-ventures/interchaintest/v7/relayer"
+	"github.com/strangelove-ventures/interchaintest/v7/testreporter"
+	"github.com/strangelove-ventures/interchaintest/v7/testutil"
+	"github.com/stretchr/testify/require"
+	"go.uber.org/zap/zaptest"
+)
+
+// TestPersistenceGaiaIBCTransfer spins up a Persistence and Gaia network, initializes an IBC connection between them,
+// and sends an ICS20 token transfer from Gaia->Persistence.
+func TestPersistenceGaiaIBCTransfer(t *testing.T) {
+	if testing.Short() {
+		t.Skip()
+	}
+
+	t.Parallel()
+
+	// Create chain factory with Persistence and Gaia
+	numVals := 1
+	numFullNodes := 1
+
+	cf := interchaintest.NewBuiltinChainFactory(zaptest.NewLogger(t), []*interchaintest.ChainSpec{
+		{
+			Name:          "persistence",
+			ChainConfig:   persistenceChainConfig(),
+			NumValidators: &numVals,
+			NumFullNodes:  &numFullNodes,
+		},
+		{
+			Name:          "gaia",
+			Version:       "v9.1.0",
+			NumValidators: &numVals,
+			NumFullNodes:  &numFullNodes,
+		},
+	})
+
+	const (
+		ibcPath = "ibc-path"
+	)
+
+	// Get chains from the chain factory
+	chains, err := cf.Chains(t.Name())
+	require.NoError(t, err)
+
+	client, network := interchaintest.DockerSetup(t)
+
+	persistenceChain, gaiaChain := chains[0].(*cosmos.CosmosChain), chains[1].(*cosmos.CosmosChain)
+
+	relayerType, relayerName := ibc.CosmosRly, "relay"
+
+	// Get a relayer instance
+	rf := interchaintest.NewBuiltinRelayerFactory(
+		relayerType,
+		zaptest.NewLogger(t),
+		interchaintestrelayer.CustomDockerImage(IBCRelayerImage, IBCRelayerVersion, "100:1000"),
+		interchaintestrelayer.StartupFlags("--processor", "events", "--block-history", "100"),
+	)
+
+	r := rf.Build(t, client, network)
+
+	ic := interchaintest.NewInterchain().
+		AddChain(persistenceChain).
+		AddChain(gaiaChain).
+		AddRelayer(r, relayerName).
+		AddLink(interchaintest.InterchainLink{
+			Chain1:  persistenceChain,
+			Chain2:  gaiaChain,
+			Relayer: r,
+			Path:    ibcPath,
+		})
+
+	ctx := context.Background()
+
+	rep := testreporter.NewNopReporter()
+	eRep := rep.RelayerExecReporter(t)
+
+	require.NoError(t, ic.Build(ctx, eRep, interchaintest.InterchainBuildOptions{
+		TestName:          t.Name(),
+		Client:            client,
+		NetworkID:         network,
+		BlockDatabaseFile: interchaintest.DefaultBlockDatabaseFilepath(),
+		SkipPathCreation:  false,
+	}))
+	t.Cleanup(func() {
+		_ = ic.Close()
+	})
+
+	// Create some user accounts on both chains
+	users := interchaintest.GetAndFundTestUsers(t, ctx, t.Name(), genesisWalletAmount, persistenceChain, gaiaChain)
+
+	// Wait a few blocks for relayer to start and for user accounts to be created
+	err = testutil.WaitForBlocks(ctx, 5, persistenceChain, gaiaChain)
+	require.NoError(t, err)
+
+	// Get our Bech32 encoded user addresses
+	persistenceUser, gaiaUser := users[0], users[1]
+
+	persistenceUserAddr := persistenceUser.FormattedAddress()
+	gaiaUserAddr := gaiaUser.FormattedAddress()
+
+	// Get original account balances
+	persistenceOrigBal, err := persistenceChain.GetBalance(ctx, persistenceUserAddr, persistenceChain.Config().Denom)
+	require.NoError(t, err)
+	require.Equal(t, math.NewInt(genesisWalletAmount), persistenceOrigBal)
+
+	gaiaOrigBal, err := gaiaChain.GetBalance(ctx, gaiaUserAddr, gaiaChain.Config().Denom)
+	require.NoError(t, err)
+	require.Equal(t, math.NewInt(genesisWalletAmount), gaiaOrigBal)
+
+	// Get Channel ID
+	gaiaChannelInfo, err := r.GetChannels(ctx, eRep, gaiaChain.Config().ChainID)
+	require.NoError(t, err)
+	gaiaChannelID := gaiaChannelInfo[0].ChannelID
+
+	var transferAmount = math.NewInt(1_000)
+
+	channel, err := ibc.GetTransferChannel(ctx, r, eRep, persistenceChain.Config().ChainID, gaiaChain.Config().ChainID)
+	require.NoError(t, err)
+
+	// Get the IBC denom for uatom on Persistence
+	gaiaTokenDenom := transfertypes.GetPrefixedDenom(channel.Counterparty.PortID, channel.Counterparty.ChannelID, gaiaChain.Config().Denom)
+	gaiaIBCDenom := transfertypes.ParseDenomTrace(gaiaTokenDenom).IBCDenom()
+
+	// Compose an IBC transfer and send from Gaia -> Persistence
+	transfer := ibc.WalletAmount{
+		Address: persistenceUserAddr,
+		Denom:   gaiaChain.Config().Denom,
+		Amount:  transferAmount,
+	}
+
+	gaiaHeight, err := gaiaChain.Height(ctx)
+	require.NoError(t, err)
+
+	transferTx, err := gaiaChain.SendIBCTransfer(ctx, gaiaChannelID, gaiaUserAddr, transfer, ibc.TransferOptions{})
+	require.NoError(t, err)
+	require.NoError(t, transferTx.Validate())
+
+	// relay MsgRecvPacket to persistence, then MsgAcknowledgement back to gaia
+	require.NoError(t, r.Flush(ctx, eRep, ibcPath, gaiaChannelID))
+
+	// Poll for the ack to know the transfer was successful
+	_, err = testutil.PollForAck(ctx, gaiaChain, gaiaHeight, gaiaHeight+25, transferTx.Packet)
+	require.NoError(t, err)
+
+	// Test destination wallet has increased funds
+	persistenceUpdateBal, err := persistenceChain.GetBalance(ctx, persistenceUserAddr, gaiaIBCDenom)
+	require.NoError(t, err)
+	require.Equal(t, transferAmount, persistenceUpdateBal)
+
+	// Test source wallet has decreased funds
+	gaiaUpdateBal, err := gaiaChain.GetBalance(ctx, gaiaUserAddr, gaiaChain.Config().Denom)
+	require.NoError(t, err)
+	require.Equal(t, gaiaOrigBal.Sub(transferAmount), gaiaUpdateBal)
+}
---
+++ interchaintest/setup.go
@@ -0,0 +1,142 @@
+package interchaintest
+
+import (
+	"context"
+	"fmt"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+	"go.uber.org/zap/zaptest"
+
+	wasmtypes "github.com/CosmWasm/wasmd/x/wasm/types"
+	testutil "github.com/cosmos/cosmos-sdk/types/module/testutil"
+	ibclocalhost "github.com/cosmos/ibc-go/v7/modules/light-clients/09-localhost"
+	liquidstaketypes "github.com/persistenceOne/pstake-native/v2/x/liquidstake/types"
+	interchaintest "github.com/strangelove-ventures/interchaintest/v7"
+	"github.com/strangelove-ventures/interchaintest/v7/chain/cosmos"
+	"github.com/strangelove-ventures/interchaintest/v7/ibc"
+	"github.com/strangelove-ventures/interchaintest/v7/testreporter"
+
+	"github.com/persistenceOne/ica-liquid-staking/interchaintest/helpers"
+)
+
+var (
+	PersistenceE2ERepo = "persistenceone/persistencecore"
+	IBCRelayerImage    = "ghcr.io/cosmos/relayer"
+	IBCRelayerVersion  = "main"
+
+	PersistenceCoreImage = ibc.DockerImage{
+		Repository: "persistence",
+		Version:    "local",
+		UidGid:     "1025:1025",
+	}
+
+	defaultGenesisOverridesKV = []cosmos.GenesisKV{
+		{
+			Key:   "app_state.gov.params.voting_period",
+			Value: "15s",
+		},
+		{
+			Key:   "app_state.gov.params.max_deposit_period",
+			Value: "10s",
+		},
+		{
+			Key:   "app_state.gov.params.min_deposit.0.denom",
+			Value: helpers.PersistenceBondDenom,
+		},
+		{
+			Key:   "app_state.builder.params.reserve_fee.denom",
+			Value: helpers.PersistenceBondDenom,
+		},
+		{
+			Key:   "app_state.builder.params.min_bid_increment.denom",
+			Value: helpers.PersistenceBondDenom,
+		},
+	}
+
+	genesisWalletAmount = int64(10_000_000)
+)
+
+// persistenceEncoding registers the persistenceCore specific module codecs so that the associated types and msgs
+// will be supported when writing to the blocksdb sqlite database.
+func persistenceEncoding() *testutil.TestEncodingConfig {
+	cfg := cosmos.DefaultEncoding()
+
+	// register custom types
+	ibclocalhost.RegisterInterfaces(cfg.InterfaceRegistry)
+	wasmtypes.RegisterInterfaces(cfg.InterfaceRegistry)
+	liquidstaketypes.RegisterInterfaces(cfg.InterfaceRegistry)
+
+	return &cfg
+}
+
+// persistenceChainConfig returns dynamic config for persistence chains, allowing to inject genesis overrides
+func persistenceChainConfig(
+	genesisOverrides ...cosmos.GenesisKV,
+) ibc.ChainConfig {
+	if len(genesisOverrides) == 0 {
+		genesisOverrides = defaultGenesisOverridesKV
+	}
+
+	config := ibc.ChainConfig{
+		Type:                   "cosmos",
+		Name:                   "persistence",
+		ChainID:                "ictest-core-1",
+		Bin:                    "persistenceCore",
+		Bech32Prefix:           "persistence",
+		Denom:                  helpers.PersistenceBondDenom,
+		CoinType:               fmt.Sprintf("%d", helpers.PersistenceCoinType),
+		GasPrices:              fmt.Sprintf("0%s", helpers.PersistenceBondDenom),
+		GasAdjustment:          1.5,
+		TrustingPeriod:         "112h",
+		NoHostMount:            false,
+		ConfigFileOverrides:    nil,
+		EncodingConfig:         persistenceEncoding(),
+		UsingNewGenesisCommand: true,
+		ModifyGenesis:          cosmos.ModifyGenesis(genesisOverrides),
+
+		Images: []ibc.DockerImage{
+			PersistenceCoreImage,
+		},
+	}
+
+	return config
+}
+
+func CreateChain(
+	t *testing.T,
+	ctx context.Context,
+	numVals, numFull int,
+	genesisOverrides ...cosmos.GenesisKV,
+) (*interchaintest.Interchain, *cosmos.CosmosChain) {
+	cf := interchaintest.NewBuiltinChainFactory(zaptest.NewLogger(t), []*interchaintest.ChainSpec{
+		{
+			Name:          "persistence",
+			ChainName:     "persistence",
+			Version:       PersistenceCoreImage.Version,
+			ChainConfig:   persistenceChainConfig(genesisOverrides...),
+			NumValidators: &numVals,
+			NumFullNodes:  &numFull,
+		},
+	})
+
+	chains, err := cf.Chains(t.Name())
+	require.NoError(t, err)
+
+	ic := interchaintest.NewInterchain().AddChain(chains[0])
+	client, network := interchaintest.DockerSetup(t)
+
+	err = ic.Build(
+		ctx,
+		testreporter.NewNopReporter().RelayerExecReporter(t),
+		interchaintest.InterchainBuildOptions{
+			TestName:         t.Name(),
+			Client:           client,
+			NetworkID:        network,
+			SkipPathCreation: true,
+		},
+	)
+	require.NoError(t, err)
+
+	return ic, chains[0].(*cosmos.CosmosChain)
+}
